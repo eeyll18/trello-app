3009f8fc289e40e5747aa682e9a0dbc8d8b5a107
eeyll18 <edaeylulgunay2@gmail.com>
2025-08-03 19:04:24 +0300

    add create board, column functionality with hook and services

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,38 @@
-export default function DashboardPage() {
-  return <div className="min-h-screen bg-gray-50">
+"use client";
 
-  </div>;
+import Navbar from "@/components/navbar";
+import { Button } from "@/components/ui/button";
+import { useBoards } from "@/lib/hooks/useBoards";
+import { useUser } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
+
+export default function DashboardPage() {
+  const { user } = useUser();
+  const {createBoard} = useBoards()
+
+  const handleCreateBoard = async () => {
+    await createBoard({title:'New Board'})
+  }
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+
+      <main className="container mx-auto px-4 py-6 sm:py-8">
+        <div className="mb-6 sm:mb-8 ">
+          <h1 className="text-2xl sm:text-3xl font-bold text-gray-900 mb-2">
+            Welcome Back,{" "}
+            {user?.firstName ?? user?.emailAddresses[0].emailAddress}
+          </h1>
+          <p className="text-gray-600">
+            Here's what's happening with your boards today.
+          </p>
+          <Button className="w-full sm:w-auto" onClick={handleCreateBoard}>
+            <Plus className="h-4 w-4 mr-2"/>
+            Create Board
+          </Button>
+        </div>
+      </main>
+    </div>
+  );
 }
