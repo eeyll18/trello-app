d47e312378c96b542457eea8c36dfbb051f938f7
eeyll18 <edaeylulgunay2@gmail.com>
2025-08-05 23:38:58 +0300

    add stats section on board page with getting tasks functionality

--- app/boards/[id]/page.tsx
+++ app/boards/[id]/page.tsx
@@ -12,7 +12,7 @@ import { FormEvent, useState } from "react";
 
 export default function BoardPage() {
   const { id } = useParams<{ id: string }>();
-  const { board, updateBoard } = useBoard(id);
+  const { board, updateBoard, columns } = useBoard(id);
 
   const [isEditingTitle, setIsEditingTitle] = useState(false);
   const [newTitle, setNewTitle] = useState("");
@@ -152,6 +152,18 @@ export default function BoardPage() {
         </DialogContent>
       </Dialog>
 
+      {/* board content */}
+
+      <main className="container mx-auto px-2 sm:px-4 py-4 sm:py-6 ">
+        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-6 space-y-4 sm:space-y-0">
+          <div className="flex flex-wrap items-center gap-4 sm:gap-6">
+            <div className="text-sm text-gray-600">
+              <span className="font-medium">Total Tasks: </span>
+              {columns.reduce((sum, col) => sum + col.tasks.length, 0)}
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
--- lib/hooks/useBoards.ts
+++ lib/hooks/useBoards.ts
@@ -3,7 +3,7 @@
 import { useUser } from "@clerk/nextjs";
 import { boardDataService, boardService } from "../services";
 import { useEffect, useState } from "react";
-import { Board, Column } from "../supabase/models";
+import { Board, Column, ColumnWithTasks } from "../supabase/models";
 import { useSupabase } from "../supabase/SupabaseProvider";
 
 export function useBoards() {
@@ -65,7 +65,7 @@ export function useBoards() {
 export function useBoard(boardId: string) {
   const { supabase } = useSupabase();
   const [board, setBoard] = useState<Board | null>(null);
-  const [columns, setColumns] = useState<Column[]>([]);
+  const [columns, setColumns] = useState<ColumnWithTasks[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
@@ -86,7 +86,7 @@ export function useBoard(boardId: string) {
         boardId
       );
       setBoard(data.board);
-      setColumns(data.columns);
+      setColumns(data.columnsWithTasks);
     } catch (error) {
       setError(
         error instanceof Error ? error.message : "Failed to load boards."
